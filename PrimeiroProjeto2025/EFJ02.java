1bfac63466628c2daaa2a28cea92d408f56850bd
Eliane9962 <eliane1992jesus@gmail.com>
2025-11-22 18:28:57 -0300

    Update EFJ02.java

--- PrimeiroProjeto2025/EFJ02.java
+++ PrimeiroProjeto2025/EFJ02.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class EFJ02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
 		System.out.println(" Projeto EFJ01 \n ");
 		System.out.println(" Aluno: Eliane Ferreira - RA:0024648 \n  ");
 		System.out.println(" Classe EFJ02 Número Par OU Ímpar!!! \n ");
-		System.out.println(" Usei atividades anteriores como base");
+
 		// ----- Fim do cabeçalho ----
 		
 		Scanner receber = new Scanner (System.in);
@@ -34,3 +33,4 @@ public class EFJ02 {
 	}
 
 }
+
